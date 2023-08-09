c588ccbd7eb8182b18c0b5f2713c87dc6344a3b1
ChenBuchuo <s1yuan_chen@163.com>
2023-08-09 15:07:34 +0800

    binarySearch basic

---
+++ src/test/java/binarysearch/TestBinarySearch.java
@@ -0,0 +1,34 @@
+package binarysearch;
+
+
+import org.example.binarysearch.BinarySearch;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestBinarySearch {
+
+    @Test
+    @DisplayName("binarySearchBasic 找到")
+    public void test1() {
+        int[] a = {7, 13, 21, 30, 38, 44, 52, 53};
+        assertEquals(0, BinarySearch.binarySearchBasic(a, 7));
+        assertEquals(1, BinarySearch.binarySearchBasic(a, 13));
+        assertEquals(2, BinarySearch.binarySearchBasic(a, 21));
+        assertEquals(3, BinarySearch.binarySearchBasic(a, 30));
+        assertEquals(4, BinarySearch.binarySearchBasic(a, 38));
+        assertEquals(5, BinarySearch.binarySearchBasic(a, 44));
+        assertEquals(6, BinarySearch.binarySearchBasic(a, 52));
+        assertEquals(7, BinarySearch.binarySearchBasic(a, 53));
+    }
+
+    @Test
+    @DisplayName("binarySearchBasic 没找到")
+    public void test2() {
+        int[] a = {7, 13, 21, 30, 38, 44, 52, 53};
+        assertEquals(-1, BinarySearch.binarySearchBasic(a, 0));
+        assertEquals(-1, BinarySearch.binarySearchBasic(a, 15));
+        assertEquals(-1, BinarySearch.binarySearchBasic(a, 62));
+    }
+}
