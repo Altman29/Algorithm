c39f76aae5dc9290b3411de71f056a3d72c1a970
ChenBuchuo <s1yuan_chen@163.com>
2023-08-11 20:26:26 +0800

    队列-数组实现

---
+++ src/main/java/org/example/datastructure/ArrayQueue1.java
@@ -0,0 +1,68 @@
+package org.example.datastructure;
+
+import java.util.Iterator;
+
+public class ArrayQueue1<E> implements Queue<E>, Iterable<E> {
+
+    private E[] array;
+    private int head;
+    private int tail;
+    private int size;
+    private int capacity;
+
+    @SuppressWarnings("all")
+    public ArrayQueue1(int capacity) {
+        array = (E[]) new Object[this.capacity + 1];
+    }
+
+    @Override
+    public boolean offer(E val) {
+        if (isFull()) return false;
+        array[tail] = val;
+        tail = (tail + 1) % array.length;
+        return true;
+    }
+
+    @Override
+    public E poll() {
+        if (isEmpty()) return null;
+        E val = array[head];
+        head = (head + 1) % array.length;
+        return val;
+    }
+
+    @Override
+    public E peek() {
+        if (isEmpty()) return null;
+        return array[head];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return head == tail;
+    }
+
+    @Override
+    public boolean isFull() {
+        return (tail + 1) % array.length == head;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            int p = head;
+
+            @Override
+            public boolean hasNext() {
+                return p != tail;
+            }
+
+            @Override
+            public E next() {
+                E e = array[p];
+                p = (p + 1) % array.length;
+                return e;
+            }
+        };
+    }
+}
